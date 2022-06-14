3cbcb69898fdf847eb58a90001b7fb059af3b153
Anna <annawkuria@gmail.com>
2022-06-14 14:43:05 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  function returnsANamedFunction(){
+    return function namedfunction(){
+        console.log ('this is a named function');
+    }
+    
+  }
+  function returnsAnAnonymousFunction(){
+    return function(){
+        console.log('this is an anonymous function');
+    }
+  }
